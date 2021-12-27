edf95328d87137f3198e57475e2153247396c91a
vputhran <vineethp70@gmail.com>
2021-12-27 13:44:29 +0530

    Day 61: Assignment Programs - Q1. Noble Integer

--- src/day61_intermediate_sorting/assignements/Q1_NobleInteger.java
+++ src/day61_intermediate_sorting/assignements/Q1_NobleInteger.java
@@ -1,5 +1,23 @@
 package day61_intermediate_sorting.assignements;
 
+/* Q1. Given an integer array A, find if an integer p exists in the array such that the number of integers greater than p in the array equals to p.
+* Example Input
+
+Input 1:
+
+ A = [3, 2, 1, 3]
+Input 2:
+
+ A = [1, 1, 3, 3]
+ * Example Output
+
+Output 1:
+
+ 1
+Output 2:
+
+ -1*/
+
 import java.util.*;
 
 public class Q1_NobleInteger {
