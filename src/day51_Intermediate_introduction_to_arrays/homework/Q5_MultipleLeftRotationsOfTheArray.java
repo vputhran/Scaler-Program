2d58ea2ec72a93592904b9f33e61054a35716461
vputhran <vineethp70@gmail.com>
2022-01-03 21:06:45 +0530

    Day 51: Homework : Q5. Multiple Left Rotation Of the Array

--- src/day51_Intermediate_introduction_to_arrays/homework/Q5_MultipleLeftRotationsOfTheArray.java
+++ src/day51_Intermediate_introduction_to_arrays/homework/Q5_MultipleLeftRotationsOfTheArray.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 
 public class Q5_MultipleLeftRotationsOfTheArray {
     public static void main(String[] args) {
+
         ArrayList<Integer> A = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
         ArrayList<Integer> B = new ArrayList<>(Arrays.asList(2, 3));
 
@@ -14,27 +15,27 @@ public class Q5_MultipleLeftRotationsOfTheArray {
     public static  ArrayList<ArrayList<Integer>> solve(ArrayList<Integer> A, ArrayList<Integer> B) {
         ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
 
-
-
         for (int i = 0; i < B.size(); i++) {
+            // Making a copy of ArrayList A
+            ArrayList<Integer> copyA = new ArrayList<>(A);
             int b = B.get(i);
-
-            mainList.add(rotateArray_ReverseLogic(A, b));
+            // Passing the copyA
+            mainList.add(rotateArray_ReverseLogic(copyA, b));
         }
         return mainList;
     }
 
-
+    // Left Shift the ArrayList
     public static ArrayList<Integer> rotateArray_ReverseLogic(ArrayList<Integer> A, int b) {
 
         b = b % A.size();
         // Reverse the entire array
         swap(A, A.size() - 1,0 );
 
-        //Reverse the Last K elements
+        //Reverse the Last K elements (Starting from the last element till the bth element from the end)
         swap(A, A.size()-1, A.size()-1-b+1);
 
-        //Reverse the remaining elements before K+1 elements
+        //Reverse the remaining elements before n+1 elements till first element
         swap(A, A.size()-1-b, 0);
 
         return A;
