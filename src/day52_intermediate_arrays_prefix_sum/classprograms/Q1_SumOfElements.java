4aa2fdd5b20bf6f8a59252bdc120567179b84e39
vputhran <vineethp70@gmail.com>
2022-01-22 12:03:47 +0530

    Day 52: Sum the elements of the array.

--- src/day52_intermediate_arrays_prefix_sum/classprograms/Q1_SumOfElements.java
+++ src/day52_intermediate_arrays_prefix_sum/classprograms/Q1_SumOfElements.java
@@ -1,5 +1,20 @@
 package day52_intermediate_arrays_prefix_sum.classprograms;
 
-public class Q1_SumOfElements {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Q1_SumOfElements {
+    public static void main(String[] args) {
+        int[] ar = {-3, 6, 2, 4, 5, 2, 8, -9, 3, 1};
+
+        int[] preAr = new int[ar.length];
+
+        preAr[0] = ar[0];
+        for (int i = 1; i < ar.length; i++) {
+            preAr[i] = preAr[i - 1] + ar[i];
+        }
+
+        Scanner sc = new Scanner(System.in);
+
+    }
 }
