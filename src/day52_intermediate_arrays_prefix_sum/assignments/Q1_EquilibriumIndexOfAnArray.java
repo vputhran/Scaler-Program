d54e0a7457d75fe9eb5a20689234c331047d8f52
vputhran <vineethp70@gmail.com>
2022-01-24 11:33:00 +0530

    Day 52: Assignment - Q1. Equilibrium Index

--- src/day52_intermediate_arrays_prefix_sum/assignments/Q1_EquilibriumIndexOfAnArray.java
+++ src/day52_intermediate_arrays_prefix_sum/assignments/Q1_EquilibriumIndexOfAnArray.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 
 public class Q1_EquilibriumIndexOfAnArray {
     public static void main(String[] args) {
-        int[] ar = {-7, 1, 5, 2, -4, 3, 0};
 
-        ArrayList<Integer> prArr = new ArrayList<Integer>(Arrays.asList(-7, 1, 5, 2, -4, 3, 0));
+        ArrayList<Integer> prArr = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
         System.out.println(solve(prArr));
 
     }
 
+    //Implementation using Array
     public static int eqiIndex(int[] preArr) {
         int lSum;
         int rSum;
@@ -25,15 +25,21 @@ public class Q1_EquilibriumIndexOfAnArray {
         return -1;
     }
 
+    // Implementation using arraylist
     public static int solve(ArrayList<Integer> A) {
         ArrayList<Integer> preSum = new ArrayList<>();
 
-        preSum.set(0, A.get(0));
+        preSum.add(A.get(0));
         for (int i = 1; i < A.size() ; i++) {
-            preSum.set(i, preSum.get(i - 1) + A.get(i));
+            preSum.add(preSum.get(i - 1) + A.get(i));
         }
         int lSum;
         int rSum;
+
+        // if- condition to handle all zero input
+        if(preSum. get(preSum. size()-1) - preSum. get(0) == 0) return 0;
+
+        // Check if lSum = rSum for values of prefix sum array
         for (int i = 1; i < preSum.size()-1; i++) {
             lSum = preSum.get(i - 1);
             rSum = preSum.get(preSum.size() - 1) - preSum.get(i);
