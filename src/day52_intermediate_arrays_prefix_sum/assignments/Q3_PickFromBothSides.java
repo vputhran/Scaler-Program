4652b5e26dbffaafe4b7d4bdc16ac35e100b800e
vputhran <vineethp70@gmail.com>
2022-01-26 20:37:06 +0530

    Day 52: Assignment - Q3 Pick from both sides

--- src/day52_intermediate_arrays_prefix_sum/assignments/Q3_PickFromBothSides.java
+++ src/day52_intermediate_arrays_prefix_sum/assignments/Q3_PickFromBothSides.java
@@ -6,24 +6,43 @@ import java.util.Arrays;
 public class Q3_PickFromBothSides {
     public static void main(String[] args) {
 
-        ArrayList<Integer> ar = new ArrayList<>(Arrays.asList(5, -2, 3, 1, 2));
-        System.out.println(solve(ar,3));
+        ArrayList<Integer> ar = new ArrayList<>(Arrays.asList(-969, -948, 350, 150, -59, 724, 966, 430, 107, -809, -993, 337, 457, -713, 753, -617, -55, -91, -791, 758, -779, -412, -578, -54, 506, 30, -587, 168, -100, -409, -238, 655, 410, -641, 624, -463, 548, -517, 595, -959, 602, -650, -709, -164, 374, 20, -404, -979, 348, 199, 668, -516, -719, -266, -947, 999, -582, 938, -100, 788, -873, -533, 728, -107, -352, -517, 807, -579, -690, -383, -187, 514, -691, 616, -65, 451, -400, 249, -481, 556, -202, -697, -776, 8, 844, -391, -11, -298, 195, -515, 93, -657, -477, 587));
+        System.out.println(solve(ar,81));
     }
 
     public static int solve(ArrayList<Integer> A, int B) {
-        int sum = 0;
-        // Prefix array
-        ArrayList<Integer> preAr = new ArrayList<>();
-        preAr.add(A.get(0));
+        int sum = Integer.MIN_VALUE;
+
+        //Creating the Prefix Sum arraylist
+        ArrayList<Integer> prefixArr = new ArrayList<>();
+        prefixArr.add(A.get(0));
         for (int i = 1; i < A.size(); i++) {
-            preAr.add(preAr.get(i - 1) + A.get(i));
+            prefixArr.add(prefixArr.get(i - 1) + A.get(i));
         }
 
-        //Suffix Array
-        ArrayList<Integer> suffixAr = new ArrayList<>();
-        suffixAr.add(A.get(A.size() - 1));
+        // Creating the suffix sum arraylist and set all the values to 0 first
+        ArrayList<Integer> suffixArr = new ArrayList<>();
+        for (int i = 0; i < A.size(); i++) {
+            suffixArr.add(0);
+        }
+        // Setting the suffix values
+        suffixArr.set(A.size() - 1, A.get(A.size() - 1));
         for (int j = A.size() - 2; j >= 0; j--) {
-            suffixAr.add(suffixAr.get(j - 1) + A.get(j));
+            suffixArr.set(j, suffixArr.get(j + 1) + A.get(j));
+        }
+        int ans;
+
+        for (int i = 0; i <= B; i++) {
+            if (i == 0) {
+                ans = prefixArr.get(B - 1);
+                sum = Math.max(sum, ans);
+            } else if (i == B) {
+                ans = suffixArr.get(suffixArr.size() - 1 - B + 1);
+                sum = Math.max(sum, ans);
+            } else {
+                ans = prefixArr.get(B - i - 1) + suffixArr.get(suffixArr.size() - i);
+                sum = Math.max(sum, ans);
+            }
         }
         return sum;
     }
