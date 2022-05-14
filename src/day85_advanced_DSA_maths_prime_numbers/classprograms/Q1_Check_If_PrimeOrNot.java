2e966e426a2b8f9a582644607a5e8ebffd87eb1c
vputhran <vineethp70@gmail.com>
2022-05-14 09:31:38 +0530

    Day 85 - Math Prime Numbers - check if number is prime or not

---
+++ src/day85_advanced_DSA_maths_prime_numbers/classprograms/Q1_Check_If_PrimeOrNot.java
@@ -0,0 +1,22 @@
+package day85_advanced_DSA_maths_prime_numbers.classprograms;
+
+public class Q1_Check_If_PrimeOrNot {
+    public static void main(String[] args) {
+        int n = -1;
+        System.out.println(isPrime(n));
+    }
+
+    public static boolean isPrime(int n) {
+        // 1 is neither prime nor composite, but assuming anything below 1 as false
+        if (n <= 1) {
+            return false;
+        }
+        // Prime numbers has only 2 factors 1 and the number itself, so any number have more than 2 factors is not prime.
+        for (int i = 2; i * i < n  ; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
