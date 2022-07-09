e786f1e47c45537bea5dcd9e5db393a73688f292
vputhran <vineethp70@gmail.com>
2022-07-09 22:58:36 +0530

    Day 81 - Advanced DSA Bit Manipulation 1 - class programs - Q1 Set bit

--- src/day81_advanced_DSA_bit_manipulations_1/classprograms/Q1_SetBit.java
+++ src/day81_advanced_DSA_bit_manipulations_1/classprograms/Q1_SetBit.java
@@ -5,7 +5,7 @@ package day81_advanced_DSA_bit_manipulations_1.classprograms;
 public class Q1_SetBit {
     public static void main(String[] args) {
         int N = 29; // 29 => 11101
-        int i = 1;
+        int i = 3;
 
         System.out.println(checkBit(N, i));
         System.out.println(checkBitRightShift(N, i));
@@ -13,15 +13,11 @@ public class Q1_SetBit {
 
     //Using Right Shift
     public static boolean checkBit(int N, int i) {
-        if (((N >> i) & 1) == 1) {
-            return true;
-        } else return false;
+        return ((N >> i) & 1) == 1;
     }
 
     //Using Left shift -
     public static boolean checkBitRightShift(int N, int i) {
-        if ((N & (1 << i)) == (1 << i)) {
-            return true;
-        } else return false;
+        return ((N & (1 << i)) == (1 << i));
     }
 }
